183e2dda4219e3389db73913b3ab786bb6b76413
istiuak <istiuak.0@gmail.com>
2026-03-21 19:00:48 +0600

    feat(monorepo): setup @aromix/core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,18 +2,19 @@ import { action } from "./action";
 import { inject, injectNew, provide } from "./di";
 import { group } from "./group";
 import { type InputSchema, input } from "./input";
-import { make } from "./make";
+import { type AromixDescriptor, make } from "./make";
 import { type Result, result } from "./result";
+
 export {
   action,
   inject,
   injectNew,
+  provide,
   input,
   make,
   group,
-  provide,
   result,
   type InputSchema,
-  type Result
+  type Result,
+  type AromixDescriptor
 };
-
