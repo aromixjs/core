2dba485d5ee629e7917299a43f5f195cd84edfcd
Istiuak <istiuak.0@gmail.com>
2026-05-16 09:21:17 +0600

    Fix core types (#11)
    * fix core types
    
    * add chageset

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,17 @@
-export * from "./build";
-export * from "./config";
-export * from "./context";
-export * from "./entity/builder";
-export * from "./entity/entity";
-export * from "./fetch/codec";
-export * from "./fetch/fetch";
-export * from "./macro";
-export * from "./make/impl";
-export * from "./plugin";
+declare global {
+    namespace Aromix {
+        type GlobPattern = string | string[];
+        function load(pattern: GlobPattern): string[];
+    }
+}
+
+export * from './build';
+export * from './config';
+export * from './context';
+export * from './entity/builder';
+export * from './entity/entity';
+export * from './fetch/codec';
+export * from './fetch/fetch';
+export * from './make/impl';
+export * from './plugin';
+
