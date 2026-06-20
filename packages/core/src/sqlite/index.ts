79fb2025cab235afec93b3d64ea0cae84e95b306
istiuak <istiuak.0@gmail.com>
2026-06-20 16:58:24 +0600

    cleanup wrong implementation

--- packages/core/src/sqlite/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-   ColumnState, ColumnType, ColumnTypeMap
-} from './states'
-export { EntityBuilder } from './entity.builder'
-export { AnyOperator, Operator, OperatorCtx, OperatorFn, AnyOperatorRecord } from './operators'
-
