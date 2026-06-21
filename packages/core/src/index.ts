06810f82b605b1ef454575f2c2ba9ebb2bb42e25
istiuak <istiuak.0@gmail.com>
2026-06-21 19:48:17 +0600

    remove ddl logic from core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,5 @@
-export * from './kv/adapter'
-export * from './kv/entity'
+export * from './kv/adapter';
+export * from './kv/entity';
+export * from './lite/adapter';
+export * from './lite/entity';
+
