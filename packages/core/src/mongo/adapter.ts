f721794de1123cc531b1ddd0e382caf95f3162ec
istiuak <istiuak.0@gmail.com>
2026-06-22 16:59:13 +0600

    update mongo adapter and entity

--- packages/core/src/mongo/adapter.ts
+++ packages/core/src/mongo/adapter.ts
@@ -1,19 +1,18 @@
 export interface MongoCollectionAdapter {
-   insertOne(doc: unknown): Promise<{ insertedId: unknown }>
-   insertMany(docs: unknown[]): Promise<{ insertedIds: unknown[] }>
-   findOne(filter: unknown): Promise<unknown | null>
-   find(filter: unknown): Promise<unknown[]>
-   updateOne(filter: unknown, update: unknown): Promise<{ matchedCount: number; modifiedCount: number }>
-   updateMany(filter: unknown, update: unknown): Promise<{ matchedCount: number; modifiedCount: number }>
-   deleteOne(filter: unknown): Promise<{ deletedCount: number }>
-   deleteMany(filter: unknown): Promise<{ deletedCount: number }>
-   countDocuments(filter?: unknown): Promise<number>
+   insertOne(doc: any): Promise<any>
+   insertMany(docs: any): Promise<any>
+   findOne(filter: any): Promise<any>
+   find(filter: any): Promise<any>
+   updateOne(filter: any, update: any): Promise<any>
+   updateMany(filter: any, update: any): Promise<any>
+   deleteOne(filter: any): Promise<any>
+   deleteMany(filter: any): Promise<any>
+   countDocuments(filter?: any): Promise<any>
 }
 
 export interface MongoAdapter {
    collection(name: string): MongoCollectionAdapter
 }
-
 
 export function MongoAdapter(adapter: MongoAdapter) {
    return adapter
