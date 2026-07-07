4d808a4f0fcc3a28795615c02d863dbea1dbf0f4
istiuak <istiuak.0@gmail.com>
2026-07-08 04:17:05 +0600

    remove mongo unit to seprate package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export { LoadEnv } from './common/LoadEnv'
-export { MongoCluster } from './common/MongoCluster'
 export { FormatLog } from './common/FormatLog'
 export { system } from './system/def'
 export { type Unit } from './system/types'
-
 export { track } from './track/def'
 export { HostCtx, context } from './track/context'
